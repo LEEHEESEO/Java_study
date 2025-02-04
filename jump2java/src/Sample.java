c5a21d645ac57e660cbc4292571a027994ebc0f9
ransom <ransom722@naver.com>
2025-02-04 20:39:12 +0900

    콘솔 입출력

--- jump2java/src/Sample.java
+++ jump2java/src/Sample.java
@@ -1,15 +1,9 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class Sample {
-    public static void main(String[] args) throws IOException{
-        InputStream in = System.in;
-        InputStreamReader reader = new InputStreamReader(in);
-        BufferedReader br = new BufferedReader(reader);
-
-        String a = br.readLine();
-        System.out.println(a);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+        sc.close();
     }
 }
